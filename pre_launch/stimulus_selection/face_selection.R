e670bb7ffa89f224e20d6c050b7c266b5ea68cac
Joel larwood <joel.larwood@uq.net.au>
2020-04-03 11:14:33 +1000

    get one set of random selections of faces

--- pre_launch/stimulus_selection/face_selection.R
+++ pre_launch/stimulus_selection/face_selection.R
@@ -18,12 +18,12 @@ library(here)
 
 # I will keep 6 examples per category. This gives 38 stimuli.
 
-emotion<- c("Neutral", 
-            "Fear", 
-            "Sadness", 
-            "Joy", 
-            "Pride", 
-            "Anger") 
+emotion <- c("Neutral", 
+             "Fear", 
+             "Sadness", 
+             "Joy", 
+             "Pride", 
+             "Anger") 
 
 
 # I will have 3 male and 3 female examples. Of one person per turn in, turn out, front on. 
@@ -40,7 +40,7 @@ direction <- c("Face Forward", "Turn Away", "Turn Forward")
 ##                      Male Randomisation                      ##
 ##################################################################
 
-male_a <- vector()
+male_faces <- vector()
 
 for (e in c(1:6)){
   for (d in c(1:3)){
@@ -48,28 +48,15 @@ for (e in c(1:6)){
     tmp_d <- direction[d]
     tmp_m <- sample(male, 1)
     tmp_file <- paste0(tmp_m, "-", tmp_e, "-", tmp_d, ".mpeg")
-    male_a <- append(male_a, tmp_file)
+    male_faces <- append(male_faces, tmp_file)
   }
 }
-
-male_b <- vector()
-
-for (e in c(1:6)){
-  for (d in c(1:3)){
-    tmp_e <- emotion[e]
-    tmp_d <- direction[d]
-    tmp_m <- sample(male, 1)
-    tmp_file <- paste0(tmp_m, "-", tmp_e, "-", tmp_d, ".mpeg")
-    male_b <- append(male_b, tmp_file)
-  }
-}
-
 
 ##################################################################
 ##                     Female Randomisation                     ##
 ##################################################################
 
-female_a <- vector()
+female_faces <- vector()
 
 for (e in c(1:6)){
   for (d in c(1:3)){
@@ -77,61 +64,35 @@ for (e in c(1:6)){
     tmp_d <- direction[d]
     tmp_f <- sample(female, 1)
     tmp_file <- paste0(tmp_f, "-", tmp_e, "-", tmp_d, ".mpeg")
-    female_a <- append(female_a, tmp_file)
+    female_faces <- append(female_faces, tmp_file)
   }
 }
 
-female_b <- vector()
+faces <- append(male_faces, female_faces)
 
-for (e in c(1:6)){
-  for (d in c(1:3)){
-    tmp_e <- emotion[e]
-    tmp_d <- direction[d]
-    tmp_f <- sample(female, 1)
-    tmp_file <- paste0(tmp_f, "-", tmp_e, "-", tmp_d, ".mpeg")
-    female_b <- append(female_b, tmp_file)
-  }
-}
+##----------------------------------
+##  Move to destination and recode  
+##----------------------------------
+library(av)
 
 
-##################################################################
-##                          Move Files                          ##
-##################################################################
+dest <- here::here(
+  "experiment", 
+  "face_judge"
+)
 
-###  Create locations                                                  
-
-a_set <- append(female_a, male_a)
-
-b_set <- append(female_b, male_b)
-
-file_loc <- "/Users/joellarwood/Desktop/ADFES"
-
-dest_a <- here::here( #set destination folder
-  "exp_files",
-  "face_a"
-) 
-
-dest_b <- here::here( #set destination folder
-  "exp_files",
-  "face_b"
-) 
-
-###  Move A set    
 
 for(i in 1:36){
-  tmp_file <- paste0("/Users/joellarwood/Desktop/ADFES/", a_set[i])
-  file.copy(from = tmp_file, 
-            to = dest_a)
-}
-
-
-###  Move B set
-
-for(i in 1:36){
-  tmp_file <- b_set[i]
-  loc_tmp <- paste0(file_loc, "/", tmp_file)
-  file.copy(from = loc_tmp, 
-            to = dest_b)
-}
-
-
+  tmp_file <- faces[i]
+  tmp_input <- paste0("/Users/joellarwood/Desktop/ADFES/", tmp_file) # set file to import
+  tmp_rename <- stringr::str_replace( #recode for output
+    tmp_file, 
+    ".mpeg", 
+    ".mp4"
+  ) 
+  tmp_out <- paste0(dest, "/", tmp_rename) #path to converted video
+  av_encode_video(
+    input = tmp_input, #imports from desktop
+    output = tmp_out, # outputs to experiment file folder 
+  )
+  
